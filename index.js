4708dc2ec4645c264883c7395223f7e2060fa621
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-08-01 12:32:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 //carregar o módulo express 
 const express = require('express')
 
+//carregar o módulo mongoose
+const mongoose = require ('mongoose')
+
+//conectar com o banco de dados
+const conexao = ()=>{
+    mongoose.connect('mongodb+srv://userRevisao:revisao@fiaptecnico.yk90q.mongodb.net/test')
+}
+
+//conectar com o bd revisao
+const modelo = new mongoose.Schema({
+    nome:String,
+    turma:String,
+    disciplina:String
+})
+const infos = mongoose.model('infos', modelo)
+
 //executar o módulo express
 const app = express()
 
@@ -9,8 +25,14 @@ app.set('views','./')
 
 //rendenizar o arquivo index.ejs na requisição 
 app.get('/',(req,res)=>{
-    res.render('index.ejs', {nome:"Alunos Feiosos (MENOS THE GIRLS)", turma:"2EMIB", disciplina:"INW"})
+    //conectar com o revisao
+    conexao()
+    //buscar todos os dados de infos
+    const resultado = infos.find()
+    res.render('index.ejs', {resultado})
 })
 
 //ligar o servidor na porta 3050
-app.listen(3050)
+app.listen(3050, ()=>{
+    console.log ('Servidor local em http://localhost:3050/')
+})
