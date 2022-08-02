7f7c3b6f111ff3ed65aa38db56c8c48288d43087
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-08-02 12:08:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//carregar o módulo express 
+//caregar o módulo express 
+const {response, urlencoded} = require('express')
 const express = require('express')
 
 //carregar o módulo mongoose
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 
-//conectar com o banco de dados
+//conectar com o banco de dados revisao 
 const conexao = ()=>{
-    mongoose.connect('mongodb+srv://userRevisao:revisao@fiaptecnico.yk90q.mongodb.net/test')
+    mongoose.connect('mongodb+srv://userRevisao:thi020206@cluster0.s3hds.mongodb.net/revisao')
 }
-
-//conectar com o bd revisao
+//conectar com a collection infos 
 const modelo = new mongoose.Schema({
     nome:String,
     turma:String,
@@ -17,22 +17,28 @@ const modelo = new mongoose.Schema({
 })
 const infos = mongoose.model('infos', modelo)
 
-//executar o módulo express
+//executar o módulo do express  
 const app = express()
 
-//definir o local padrão para os arquivos  ejs
+//definir o local padrão para os arquivos ejs
 app.set('views','./')
 
-//rendenizar o arquivo index.ejs na requisição 
-app.get('/',(req,res)=>{
-    //conectar com o revisao
+//renderizar o arquivo index.ejs na requisição / (root)
+app.get('/',async(req,res)=>{
+    //conectar com o revisao 
     conexao()
     //buscar todos os dados de infos
-    const resultado = infos.find()
-    res.render('index.ejs', {resultado})
+    const resultado = await infos.find()
+    res.render('index.ejs',{resultado})
+})
+
+app.use(urlencoded({extend:false}))
+app.post('/', async(req,res)=>{
+    const dados = req.body
+    res.send(dados)
 })
 
 //ligar o servidor na porta 3050
 app.listen(3050, ()=>{
-    console.log ('Servidor local em http://localhost:3050/')
+    console.log('servidor local em http://localhost:3050')
 })
